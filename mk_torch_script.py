095bc5faea348c00051df8d7115f3267b36c2a3f
Yongjin Jo <tmspr3693@naver.com>
2025-01-23 17:01:23 +0900

    Update mk_torch_script.py

--- mk_torch_script.py
+++ mk_torch_script.py
@@ -1,70 +1,13 @@
-# ------------------------------------------------------------------------------
-# Copyright (c) Microsoft
-# Licensed under the MIT License.
-# Written by Ke Sun (sunk@mail.ustc.edu.cn)
-# ------------------------------------------------------------------------------
-import math
 import os
 import pprint
-import _init_paths
-from lib.core.Counter import Counter
+
 from lib.utils.utils import create_logger, random_seed_setting
-from lib.utils.modelsummary import get_model_summary
-from lib.core.cc_function import test_cc
-import datasets
-import torch.backends.cudnn as cudnn
 import torch.nn as nn
 import torch
-import numpy as np
-import timeit
-import time
-import logging
 import argparse
 from lib.models.build_counter import Baseline_Counter
-from lib.utils.dist_utils import (
-    get_dist_info,
-    init_dist)
 from mmcv import Config, DictAction
-from functools import partial
-
-import cv2
-import tqdm
-import matplotlib.pyplot as plt
-import pickle
 from torch.nn import functional as F
-
-def pad_image( image, h, w, size, padvalue):
-    pad_image = image.copy()
-    pad_h = max(size[0] - h, 0)
-    pad_w = max(size[1] - w, 0)
-    if pad_h > 0 or pad_w > 0:
-        pad_image = cv2.copyMakeBorder(image, 0, pad_h, 0,
-            pad_w, cv2.BORDER_CONSTANT,
-            value=padvalue)
-    
-    return pad_image
-
-def check_img(image,divisor ):
-    h, w = image.shape[:2]
-    if h % divisor != 0:
-        real_h = h+(divisor - h % divisor)
-    else:
-        real_h = h
-    if w % divisor != 0:
-        real_w = w+(divisor - w % divisor)
-    else:
-        real_w = 0
-    image = pad_image(image, size=(real_h, real_w), h=h,w=w, padvalue= (0.0, 0.0, 0.0))
-    return image
-
-def input_transform(image):
-    mean=[0.485, 0.456, 0.406]
-    std=[0.229, 0.224, 0.225]
-    image = image.astype(np.float32)[:, :, ::-1]
-    image = image / 255.0
-    image -= mean
-    image /= std
-    return image
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Test crowd counting network')
@@ -102,15 +45,6 @@ def parse_args():
         os.environ['LOCAL_RANK'] = str(args.local_rank)
 
     return args
-
-# img = "/home/cho092871/Desktop/Datasets/Itaewon_ver2.0/imgs/CCTV-views/IMG_1000.jpeg"
-# cv_image = cv2.imread(img, cv2.IMREAD_COLOR)
-# height, width, _ = cv_image.shape
-# cv_image = cv2.resize(cv_image, dsize=(width//4, height//4))
-# cv_image = check_img(cv_image, 32)
-# input_image = input_transform(cv_image)
-# input_image = input_image.transpose((2, 0, 1))
-# input_image = torch.from_numpy(input_image).unsqueeze(0)
 
 class ScriptModule(nn.Module):
     def __init__(self, base_model):
@@ -156,7 +90,6 @@ class ScriptModule(nn.Module):
 
         return ori_den
 
-        
 
 def main():
     args = parse_args()
